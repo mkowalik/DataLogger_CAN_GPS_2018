0aac3eeece98aae53317d8fdd1b9ad7422721d0b
Michał Kowalik <kowalik.michal1@gmail.com>
2019-01-19 17:32:50 +0100

    Bug fixing in functionality of reading raw int or unsigned int byte by byte.

--- DataLoggerPC/AGHData/raw_data_parser.cpp
+++ DataLoggerPC/AGHData/raw_data_parser.cpp
@@ -7,34 +7,36 @@ using namespace std;
 RawDataParser::RawDataParser(EndianessMode rawDataMode) : rawDataMode(rawDataMode) {
 }
 
-int RawDataParser::interpret_signed_int(char* raw_data, unsigned int bytes) const {  //TODO mocno przetestować! //TODO dlaczego nie zalezy to od mode!!!
+int RawDataParser::interpret_signed_int(char* raw_data, int bytes) const {  //TODO mocno przetestować! //TODO dlaczego nie zalezy to od mode!!!
 
-    bytes = min(bytes, 4u);
+    bytes = min(bytes, static_cast<int>(sizeof(int)));
+
+    int extra_shift = static_cast<int>(sizeof(int)) - bytes;
 
     int retNumber = 0;
 
-    for (unsigned int i=0; i<bytes; i++){
-        retNumber |= ((static_cast<unsigned int>(*(raw_data+i))) << (i*8));
-    }
-
-
-    if (raw_data[0] < 0){
-        for (unsigned int i=3; i>=bytes; i--){
-            retNumber |= ((0xFF) << (i*8));
+    if (rawDataMode == LittleEndian){
+        for (int i=0; i<bytes; i++){
+            retNumber |= ((static_cast<unsigned char>(raw_data[i])) << ((i+extra_shift)*8));
         }
     }
+
+    retNumber >>= (extra_shift*8);
 
     return retNumber;
 }
 
-unsigned int RawDataParser::interpret_unsigned_int(char* raw_data, unsigned int bytes) const {
+unsigned int RawDataParser::interpret_unsigned_int(char* raw_data, int bytes) const {
 
-    bytes = min(bytes, 4u);
+    int size_of_uint = sizeof(unsigned int);
+    bytes = min(bytes, size_of_uint);
 
     unsigned int retNumber = 0;
 
-    for (unsigned int i=0; i<bytes; i++){
-        retNumber |= ((static_cast<unsigned int>(*(raw_data+i))) << (i*8));
+    if (rawDataMode == LittleEndian){
+        for (int i=0; i<bytes; i++){
+            retNumber |= ((static_cast<unsigned char>(raw_data[i])) << (i*8));
+        }
     }
 
     return retNumber;
--- DataLoggerPC/AGHData/raw_data_parser.h
+++ DataLoggerPC/AGHData/raw_data_parser.h
@@ -9,8 +9,8 @@ public:
 //        BigEndian //TODO
     };
     RawDataParser(EndianessMode rawDataMode);
-    int             interpret_signed_int(char* raw_data, unsigned int bytes) const;
-    unsigned int    interpret_unsigned_int(char* raw_data, unsigned int bytes) const;
+    int             interpret_signed_int(char* raw_data, int bytes) const;
+    unsigned int    interpret_unsigned_int(char* raw_data, int bytes) const;
 
     void            write_signed_int(int value, char* retBuffer, unsigned int bytes);
     void            write_unsigned_int(unsigned int value, char* retBuffer, unsigned int bytes);
